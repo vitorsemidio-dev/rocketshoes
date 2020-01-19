6120dc846db8aeddb8e3e29b47c7b55c9d199647
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-18 23:58:48 -0300

    Consumindo dados json-server

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img src="" alt="Tênis"/>
-      </li>
-      <strong>Tênis de corrida</strong>
-      <span>R$159,00</span>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-      <button type="button">
-        <div>
-          <MdAddShoppingCart size={16} color="#FFF"/> 1
-        </div>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-      <span>Adicionar ao Carrinho</span>
-      </button>
-    </ProductList>
-  );
+    this.setState({ products: response.data });
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.price}</span>
+
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" /> 1
+              </div>
+
+              <span>Adicionar ao Carrinho</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
