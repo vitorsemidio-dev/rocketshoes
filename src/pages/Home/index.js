d59700437e3fb7df855380685a39c5e31c8cedc6
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-18 22:42:52 -0300

    Estrutura Home (Lista de produtos)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home Component</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img src="" alt="Tênis"/>
+      </li>
+      <strong>Tênis de corrida</strong>
+      <span>R$159,00</span>
+
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#FFF"/> 1
+        </div>
+
+      <span>Adicionar ao Carrinho</span>
+      </button>
+    </ProductList>
+  );
 }
