93140f6e4d753d9aab68145fb7da30d54f3cbcc3
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-18 23:10:08 -0300

    Estrutura cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,61 @@
 import React from 'react';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Cart Component</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src="" alt="Tênis"/>
+            </td>
+            <td>
+              <strong>Tênis de corrida</strong>
+              <span>R$159,90</span>
+            </td>
+            <td>
+              <button type="button">
+                <MdRemoveCircleOutline size={20} color="#7159c1" />
+              </button>
+              <input type="number" readOnly value={2}/>
+              <button type="button">
+                <MdAddCircleOutline size={20} color="#7159c1" />
+              </button>
+            </td>
+            <td>
+              <strong>R$318,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+
+      </ProductTable>
+
+      <footer>
+        <button type="button">
+          Finalizar Pedido
+        </button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$2000,00</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const ProductTable = styled.div``;
+
+export const Total = styled.div``;
