a0e834e622d9235bf7c0da50ca00ebc29ccd4c3c
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-20 15:58:39 -0300

    Qtd cada modelo de tênis adicionado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -34,6 +34,8 @@ class Home extends Component {
 
   render() {
     const { products } = this.state;
+    const { amount } = this.props;
+
     return (
       <ProductList>
         {products.map(product => (
@@ -47,7 +49,8 @@ class Home extends Component {
               onClick={() => this.handleAddProduct(product)}
             >
               <div>
-                <MdAddShoppingCart size={16} color="#FFF" /> 1
+                <MdAddShoppingCart size={16} color="#FFF" />{' '}
+                {amount[product.id] || 0}
               </div>
 
               <span>Adicionar ao Carrinho</span>
@@ -62,4 +65,11 @@ class Home extends Component {
 const mapDispatchToProps = dispatch =>
   bindActionCreators(CartActions, dispatch);
 
-export default connect(null, mapDispatchToProps)(Home);
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+    return amount;
+  }, {}),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
