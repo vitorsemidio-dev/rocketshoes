96184224d474c7c1442fc336d992e0d6f2e78b87
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-20 09:11:00 -0300

    dispatch

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -23,6 +23,15 @@ class Home extends Component {
     this.setState({ products: data });
   }
 
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
+  };
+
   render() {
     const { products } = this.state;
     return (
@@ -33,7 +42,10 @@ class Home extends Component {
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
 
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
                 <MdAddShoppingCart size={16} color="#FFF" /> 1
               </div>
